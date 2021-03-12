38fef34add64069d94c0af886e89c905845f5341
Dex <k.decker@elsevier.com>
2021-03-12 16:12:18 +0000

    Format output

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const core = require('@actions/core');
 const axios = require('axios');
+const AsciiTable = require('ascii-table')
 
 try {
     const URL = 'https://slack.com/api/chat.postMessage';
 
+    const message = core.getInput('message');
+    const tableInfo = message[0];
+    const table = message[1].data;
+    const tableHeader = table.head;
+    const tableBody = table.body;
+
+    var formattedTable = new AsciiTable('Out-of-date dependencies');
+    formattedTable.setHeading(tableHeader);
+    tableBody.forEach(tableRow => { formattedTable.addRow(tableRow)});
 
     axios.post(URL, {
-        "text": core.getInput('message'),
+        "text": formattedTable.toString(),
         "channel": core.getInput('channel_id'),
     },
         {
