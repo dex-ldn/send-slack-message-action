7abf710a048de7ed1064496cb35d5c4337c5804f
Dex <k.decker@elsevier.com>
2021-03-12 14:55:06 +0000

    First try

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+try {
+    const URL = 'https://slack.com/api/chat.postMessage';
+    const message = 'FOOBAR';
+
+    axios.post(URL, {
+        message,
+        channel: "C01R1KE835Y",
+    },
+        {
+           headers: {
+                "Content-Type": 'application/json',
+                Authorization: 'Bearer ' + core.getInput('SLACK_BOT_TOKEN')
+            }
+        }
+    )
+    .then(function (response) {
+        console.log(response);
+    })
+    
+
+} catch (error) {
+  core.setFailed(error.message);
+}
